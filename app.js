57c1f504555712a6149cc0fdde598d145816d30c
Michael Yang <ledongyang@gmail.com>
2017-07-31 18:32:34 -0400

    up to rendering the template

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 const app = express();
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views');
+
+// nunjucks.configure('views', {noCache: true});
+// nunjucks.render('index.html', locals, function (err, output) {
+//     console.log(output);
+// });
 
 app.use('/', function(req, res, next){
   console.log(req.method, req.url)
