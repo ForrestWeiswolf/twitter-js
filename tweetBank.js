e7236dcdecdc1bc765e72c49de4685fb738ae216
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-08-01 11:46:12 -0400

    Made fake names nonrandom

--- tweetBank.js
+++ tweetBank.js
@@ -20,10 +20,10 @@ const randArrayEl = function(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
-const getFakeName = function() {
+const getFakeName = function(i) {
   const fakeFirsts = ['Nimit', 'David', 'Shanna', 'Emily', 'Scott', 'Karen', 'Ben', 'Dan', 'Ashi', 'Kate', 'Omri', 'Gabriel', 'Joe', 'Geoff'];
   const fakeLasts = ['Hashington', 'Stackson', 'McQueue', 'OLogn', 'Ternary', 'Claujure', 'Dunderproto', 'Binder', 'Docsreader', 'Ecma'];
-  return randArrayEl(fakeFirsts) + " " + randArrayEl(fakeLasts);
+  return fakeFirsts[i] + " " + fakeLasts[i];
 };
 
 const getFakeTweet = function() {
@@ -32,5 +32,5 @@ const getFakeTweet = function() {
 };
 
 for (let i = 0; i < 10; i++) {
-  module.exports.add( getFakeName(), getFakeTweet() );
+  module.exports.add( getFakeName(i), getFakeTweet() );
 }
