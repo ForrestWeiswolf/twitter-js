6d7ad13906b4fb839ea143ad387b351a2f61835c
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-08-01 11:49:29 -0400

    Can display all the tweets for a specific user

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,12 @@ const tweetBank = require('../tweetBank');
 
 router.use(express.static('public'))
 
+router.get('/users/:name', function(req, res) {
+  let name = req.params.name;
+  let tweets = tweetBank.find( {name: name} );
+  res.render( 'index', { tweets: tweets } );
+});
+
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
   res.render( 'index', { tweets: tweets } );
