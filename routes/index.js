62141b3c52537c44abe87766f479974acedf1a40
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-08-01 15:02:49 -0400

    Dynamically pre-populating fields

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,10 @@ const tweetBank = require('../tweetBank');
 router.use(express.static('public'))
 
 router.get('/users/:name', function(req, res) {
-  let name = req.params.name;
-  let tweets = tweetBank.find( {name: name} );
-  res.render( 'index', { tweets: tweets } );
+  const decodedName = decodeURI(req.params.name)
+  console.log("Decoded:", decodedName)
+  let tweets = tweetBank.find( {name: req.params.name} );
+  res.render( 'index', { tweets: tweets, showForm : true, name: decodedName});
 });
 
 router.get('/tweets/:id', function(req, res) {
@@ -25,7 +26,12 @@ router.post('/tweets', function(req, res) {
 
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets, showForm : true} );
+
+  tweets.forEach(function(tweet){
+    tweet.encodedName = encodeURI(tweet.name)
+  });
+
+  res.render( 'index', { tweets: tweets, showForm : true, name: ''} );
 });
 
 module.exports = router;
